6d1b9002e976341d99f0b4e0d0405bc9da9060c3
kritapas-phoenix <kritapas.phoenix@gmail.com>
2025-06-04 10:02:14 +0700

    Create 02-PythonInputOutput.py

---
+++ LAB/03-PythonVariables.py
@@ -0,0 +1,8 @@
+Team1 = "Liverpool"
+Team2 = "Chelsea"
+Score1 = "4"
+Score2 = "3"
+print (Team1, " versus ", Team2,)
+print ("Final score ", Score1, " to ", Score2)
+print(f'{Team1} versus {Team2}')
+print(f'{Score1} to {Score2}')
